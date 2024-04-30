f9ad803c4449f67c0d67cf49ea8ff30d402b976f
Shuyuan <kukutou_mikiya@outlook.com>
2024-04-29 22:50:01 -0400

    init commit

---
+++ src/main/java/com/itheima/springbootmanagesys/SpringbootManageSysApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootmanagesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManageSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootManageSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheima/springbootmanagesys/SpringbootManageSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootmanagesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootManageSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
